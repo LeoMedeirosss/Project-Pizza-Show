caa400696cd13dcd86c61b70940a8b8559f49b20
LeoMedeirosss <lmf@cesar.school>
2025-08-28 18:58:14 -0300

    feat: helmet

--- Pizza-Shop-Project/src/App.tsx
+++ Pizza-Shop-Project/src/App.tsx
@@ -1,11 +1,16 @@
 import "./global.css"
 
 import { RouterProvider } from "react-router-dom"
+import { Helmet, HelmetProvider } from "react-helmet-async"
+
 import { router } from "./routes"
 
 export function App() {
 
   return (
+    <HelmetProvider>
+      <Helmet titleTemplate="%s | pizza.shop" />
       <RouterProvider router={router} />
+    </HelmetProvider>
   )
 }
